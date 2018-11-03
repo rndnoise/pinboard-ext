934e2e9f49ea94aa0d153b79086a9a33273f031e
rndnoise <34294193+rndnoise@users.noreply.github.com>
2018-11-03 14:59:08 -0500

    move watch.sh to build/ directory

--- build/watch.sh
+++ build/watch.sh
@@ -2,10 +2,10 @@
 pulp \
   --watch \
   --before clear \
-  --then "say holy crow && ./build.sh" \
+  --then "say holy crow && $(dirname $0)/build.sh" \
   --else "say it doesn\'t like it" \
   build \
   --jobs 2 \
   -- --stash
 
-  --then "./build.sh && say holy crow" \
+--then "$(dirname $0)/build.sh && say holy crow" \
